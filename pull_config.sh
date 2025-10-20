b5b3ae34b82c5a27be009a56999b8cb3f05907a9
AL3X-69 <26258257+AL3X-69@users.noreply.github.com>
2025-10-20 11:41:18 +0200

    Added rofi config

--- pull_config.sh
+++ pull_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a config_dirs=(".config/hypr" ".config/waybar" ".config/nvim" ".config/wpaperd")
+declare -a config_dirs=(".config/hypr" ".config/waybar" ".config/nvim" ".config/wpaperd", ".config/rofi", ".local/share/rofi")
 
 for dir in "${config_dirs[@]}" 
 do
