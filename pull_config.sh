4cae8225a8529a1d44a9dab7d4abb7e4045723fa
AL3X-69 <26258257+AL3X-69@users.noreply.github.com>
2025-10-20 11:55:26 +0200

    Fixed config import script

--- pull_config.sh
+++ pull_config.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-declare -a config_dirs=(".config/hypr" ".config/waybar" ".config/nvim" ".config/wpaperd", ".config/rofi", ".local/share/rofi")
+declare -a config_dirs=(
+  ".config/hypr"
+  ".config/waybar"
+  ".config/nvim"
+  ".config/wpaperd"
+  ".config/rofi"
+  ".local/share/rofi"
+)
 
-for dir in "${config_dirs[@]}" 
-do
-    rsync -Rav --delete $HOME/./$dir . 
+for dir in "${config_dirs[@]}"; do
+    rsync -Rav --delete "$HOME/./$dir" .
 done
