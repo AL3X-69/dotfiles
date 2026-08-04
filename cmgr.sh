91e0acef50bb10afaece9debe7e0d342d1663840
AL3X-69 <26258257+AL3X-69@users.noreply.github.com>
2026-08-04 16:33:10 +0200

    Added ly config

--- cmgr.sh
+++ cmgr.sh
@@ -12,10 +12,19 @@ declare -a config_dirs=(
     ".local/share/rofi"
 )
 
+declare -a sudo_config=(
+    "/etc/ly/config.ini"
+)
+
 fn_pull() {
     for dir in "${config_dirs[@]}"; do
         printf "\nPulling from $dir\n"
         rsync -Rav --delete "$HOME/./$dir" .
+    done
+    # We dont need sudo as the system wide config files stored are not sensible and have the other read perm
+    for dir in "${sudo_config[@]}"; do
+        printf "\nPulling from $dir\n"
+        rsync -Rav --delete "$dir" system
     done
 }
 
@@ -24,6 +33,14 @@ fn_push() {
         printf "\nPushing to $dir\n"
         rsync -Rav "./$dir" "$HOME/"
     done
+    if [[ $EUID -ne 0 ]]; then
+        printf "\nScript not run as root, ignoring system-wide config files\n"
+    else
+        for dir in "${config_dirs[@]}"; do
+            printf "\nPushing to $dir\n"
+            rsync -Rav "system$dir" "/"
+        done
+    fi
 }
 
 if [[ "$1" == "pull" ]]; then
